8f5e2fc1cd454410cddaeb68c14a69e49d086583
setroc <isaaccortes99@outlook.com>
2022-01-04 21:40:55 -0600

    [EDIT] vinculos

--- admin/header.php
+++ admin/header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../src/css/app.css">
-    <link rel="stylesheet" href="../src/css./admin.css">
+    <link rel="stylesheet" href="../src/css/admin.css">
     <title>Classroom - ADMIN</title>
 </head>
 <body>
